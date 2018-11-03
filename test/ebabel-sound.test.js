ff04351e28c4c59ff59b13557dd4a98afd3e4e4a
Thomas Amar <hello@ebabel.eu>
2018-11-03 19:25:25 +0100

    Write Sound class.

--- test/ebabel-sound.test.js
+++ test/ebabel-sound.test.js
@@ -1,17 +1,8 @@
 'use strict';
 
-const { mockTHREE } = require('ebabel-mocks');
-const ebabelSound = require('../src/ebabel-sound.js');
-
-let THREE;
-let scene;  /* eslint no-unused-vars:0 */
-
-beforeEach(() => {
-  THREE = mockTHREE;
-  scene = new THREE.Scene();
-});
+const Sound = require('../src/ebabel-sound.js');
 
 test('ebabelSound returns something other than undefined.', () => {
-  const result = ebabelSound();
+  const result = new Sound();
   expect(result !== undefined).toBe(true);
 });
